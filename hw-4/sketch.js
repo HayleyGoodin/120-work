e732e81baf7a596bee481fa5882f0c2806f5a11b
Hayley <hayley.goodin@umconnect.umt.edu>
2018-09-17 01:24:21 -0600

    Hw-4 Submission

---
+++ hw-4/sketch.js
@@ -0,0 +1,49 @@
+function setup() {
+
+    createCanvas( 230, 300 );
+
+    background( 'grey' );
+  }
+
+function draw() {
+  text("I approve of ellipse", 60, 30);
+// left and right from your point of view
+  ellipse( 125, 220, 50, 50); // tail butt (for depth)
+  ellipse( 150, 230, 130, 30); //tail
+  ellipse( 67, 220, 50, 50); // left leg
+  ellipse( 50, 235, 40, 28); // left foot
+  ellipse( 100, 190, 80, 100); // body
+  ellipse( 100, 120, 90, 80); // head
+  ellipse( 80, 112, 30, 30 ); // left eye
+  ellipse( 120, 112, 30, 30 ); // right eye
+  ellipse( 80, 112, 2, 2); // left pupil
+  ellipse( 120, 112, 2, 2); // right pupil
+  ellipse( 100, 120, 5, 5 ); // tiny nose
+  ellipse( 100, 140, 70, 1); // mouth
+  ellipse( 80, 143, 2, 5); // left tooth
+  ellipse( 120, 143, 2, 5); // right tooth
+  ellipse( 70, 70, 10, 40); // left ear
+  ellipse( 130, 70, 10, 40); // right ear
+  ellipse( 140, 170, 50, 20); // right arm
+  ellipse( 165, 170, 27, 20); // right hand
+  ellipse( 157, 166, 10, 25); // right hand thumbs up
+  ellipse( 168, 170, 22, 20); // right hand finger top layer
+  ellipse( 173, 168, 10, 1); // right hand finger top
+  ellipse( 172, 174, 12, 1); // right hand finger bottom
+  ellipse( 120, 225, 50, 50); //right leg
+  ellipse( 125, 240, 30, 40); // right foot
+  ellipse( 74, 180, 20, 40); // left arm
+  ellipse( 76, 188, 20, 35); // left hand
+  ellipse( 71, 199, 1, 8); // left finger far left
+  ellipse( 75, 200, 1, 8); // left finger middle
+  ellipse( 80, 199, 1, 8); // left finger far right
+  ellipse( 125, 244, 10, 12); // right paw pad
+  ellipse( 117, 234, 6, 6); // right paw pad left toe
+  ellipse( 125, 230, 6, 6); // right paw pad middle toe
+  ellipse( 133, 234, 6, 6); // right paw pad right toe
+  ellipse( 54, 235, 12, 10); // left paw pad
+  ellipse( 41, 235, 5, 5); // left paw pad middle toe
+  ellipse( 45, 242, 5, 5); // left paw pad bottom toe
+  ellipse( 45, 228, 5, 5); // left paw pad top toe
+
+}
