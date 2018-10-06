a33eff24f08b75850399572f7885a0d1d5175168
Hayley <hayley.goodin@umconnect.umt.edu>
2018-10-06 00:24:37 -0600

    Hw-6

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -3,36 +3,42 @@ var B = 700;
 var C = 0;
 var D = 0;
 var E = 255;
+var bounce1 = 18;
+var bounce2 = 6;
 
 
 function setup() {
 
     createCanvas( windowWidth, windowHeight );
-    frameRate(60);
+    frameRate(24);
 
-    background( D, 0, E );
+
   }
 
 function draw() {
 
-     noStroke ();
-     fill( 'black');
-     D = map(mouseX, 0, windowWidth, 0 , 256); // change colors
-     E = map(mouseY, windowHeight, 0, 70, 0); // change colors
-     ellipse(mouseX, mouseY, 40, 40);
-     if (mouseIsPressed) {
-      background( D, 0, E ); //refresh art
-     } else {
-       ellipse(mouseX, mouseY, 40, 40);
-     }
-    
+     background( D, 0, E );
+
+     D = map(mouseX, 0, windowWidth, 0 , 256); // change colors map1
+     E = map(mouseY, windowHeight, 0, 70, 0); // change colors map2
+
+
+
      fill( 'yellow' );
-     rect(600, B, 70, 70); // box up
-     B = B - 9
+     rect(400, B, 70, 70); // box up
 
-     fill( 'red' );
-     rect(C, 600, 70, 70); // box right
-     C = C + 18
+     if ( B > height ) {
+     bounce2 = -6;
+     }
 
+     B = B + bounce2;
 
+     fill( 'yellow' );
+     ellipse(C, 600, 90, 90); // box right
+
+     if ( C > width ) {
+     bounce1 = -18;
+     }
+
+     C = C + bounce1;
 }
